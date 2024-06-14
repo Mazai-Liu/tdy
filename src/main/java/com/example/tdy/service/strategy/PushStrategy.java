9c5992f2ec0b98bd6b23ea26fc448b63332aa23f
Mazai-Liu <64641934+Mazai-Liu@users.noreply.github.com>
2024-06-14 23:05:43 +0800

    完成异步推视频
    * bugfix
    
    * 初步完成feed策略拉模式重构
    
    * 完善了策略模式代码
    
    * 完成热度排行榜和热度视频代码，等待调试。运用了堆和定时任务
    
    * 删了部分用不到的库
    
    * 完成热门视频和热门视频排行榜测试
    
    * 修改为FastJSON和Gson安全版本
    
    * 完成异步推视频

--- src/main/java/com/example/tdy/service/strategy/PushStrategy.java
+++ src/main/java/com/example/tdy/service/strategy/PushStrategy.java
@@ -3,6 +3,7 @@ package com.example.tdy.service.strategy;
 import com.example.tdy.entity.Video;
 import com.example.tdy.service.FollowService;
 import com.example.tdy.utils.RedisUtil;
+import com.example.tdy.utils.ThreadPoolUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
@@ -22,6 +23,9 @@ public class PushStrategy implements FeedStrategy {
 
     @Autowired
     private RedisUtil redisUtil;
+
+    @Autowired
+    private ThreadPoolUtil threadPoolUtil;
   
     @Override
     public void initFollowFeed(Integer userId, List<Integer> followIds) {
@@ -31,12 +35,14 @@ public class PushStrategy implements FeedStrategy {
 
     @Override
     public void onVideoPublish(Video video) {
-        // 推模式中，发布的视频要推送到关注者的收件箱
-        Integer userId = video.getUserId();
-        // 关注者id
-        List<Integer> follows = followService.getFollows(userId, null);
+        threadPoolUtil.submit(() -> {
+            // 推模式中，发布的视频要推送到关注者的收件箱
+            Integer userId = video.getUserId();
+            // 关注者id
+            List<Integer> follows = followService.getFollows(userId, null);
 
-        redisUtil.addInbox(follows, video);
+            redisUtil.addInbox(follows, video);
+        });
     }
 
 }
